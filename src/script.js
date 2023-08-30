8334cc3072b593e32d9b0156bfb8d48046ab011f
rafallyczek <rafal.lyczek@gmail.com>
2023-08-30 10:55:24 +0200

    Implemented nav toggle
    Added functionality to show/hide navigation bar.

--- src/script.js
+++ src/script.js
@@ -1,12 +1,16 @@
 //Imports
 import './style.css';
+import "@fortawesome/fontawesome-free/js/fontawesome.js";
+import "@fortawesome/fontawesome-free/js/solid.js";
 
 //DOM
 const dividend = document.querySelector("#dividend");
 const divisor = document.querySelector("#divisor");
 const calculateBtn = document.querySelector("#calculate");
-const result = document.querySelector("#result");
+const navToggleBtn = document.querySelector(".nav-toggle");
 
+const content = document.querySelector(".content");
+const navContent = document.querySelector(".nav-content");
 const value = document.querySelector("#value");
 const incrementBtn = document.querySelector("#increment");
 const decrementBtn = document.querySelector("#decrement");
@@ -15,6 +19,7 @@ const decrementBtn = document.querySelector("#decrement");
 calculateBtn.addEventListener("click", calculate);
 incrementBtn.addEventListener("click", increment);
 decrementBtn.addEventListener("click", decrement);
+navToggleBtn.addEventListener("click", toggleNav);
 
 //Functions
 function calculate() {
@@ -54,6 +59,32 @@ function isEmpty() {
 
 function saveData() {
   localStorage.setItem("counter", +value.textContent);
+}
+
+//Display
+function toggleNav() {
+
+  const icon = document.createElement("i");
+  icon.classList.add("fa-solid");
+  icon.classList.add("fa-2xl");
+
+  if(content.classList.contains("nav-expanded")){
+    content.classList.remove("nav-expanded");
+    content.classList.add("nav-hidden");
+    icon.classList.add("fa-caret-right");
+    navToggleBtn.title = "Show navigation";
+    navContent.style.display = "none";
+  } else {
+    content.classList.remove("nav-hidden");
+    content.classList.add("nav-expanded");
+    icon.classList.add("fa-caret-left");
+    navToggleBtn.title = "Hide navigation";
+    navContent.style.display = "block";
+  }
+
+  navToggleBtn.textContent = "";
+  navToggleBtn.appendChild(icon);
+
 }
 
 init();
